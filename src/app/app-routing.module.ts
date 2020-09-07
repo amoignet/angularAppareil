31b3bbd2f348716e6e91367993acc23168c0ea1f
amoignet <antoinemoignet@gmail.com>
2020-09-07 12:57:56 -0400

    add template form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuardService } from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 const routes: Routes = [
   { path: 'auth',  component: AuthComponent },
   { path: 'appareils', canActivate: [AuthGuardService], component: AppareilViewComponent },
   { path: 'appareils/:id', canActivate: [AuthGuardService], component: SingleAppareilComponent },
+  { path: 'edit', canActivate: [AuthGuardService], component: EditAppareilComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'notFound', component: FourOhFourComponent },
   { path: '**', redirectTo: 'notFound' }
